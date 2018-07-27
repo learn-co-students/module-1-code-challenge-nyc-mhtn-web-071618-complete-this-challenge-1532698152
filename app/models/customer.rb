0c563c8a716c2a16447e032da3e68c62d6431a98
github username <github email address>
2018-07-27 10:32:09 -0400

    terry

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,58 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def find_by_name(name)
+    #given a string of a **full name**, returns the **first customer** whose full name matches
+    name.find do |names|
+      names == self.full_name
+    end
+  end
+
+  def find_all_by_first_name(name)
+    @@all.select do |names|
+      names.first_name == name
+    end
+  end
+
+  def all_names
+    array = []
+    @@all.map do |names|
+      array << names.join
+    end
+  end
+
+  def add_review(restaurant, content, rating)
+    Restaurant.new(rating, content, self, restaurant)
+  end
+
+  def num_reviews
+    Review.all.select do |reviews|
+      reviews.customer == self
+      ##needs a .count/.length in here
+    end
+  end
+
+  def restaurants
+    Review.all.select do |reviews|
+      reviews.customer == self
+      reviews.restaurant
+    end
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,41 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def find_by_name(name)
+    @@all.find do |restaurants|
+      restaurants.name == name
+    end
+  end
+
+  def reviews
+    Review.all.select do |reviews|
+      reviews.restaurant == self
+  end
+end
+
+  def customer
+    Review.all.select do |reviews|
+    end
+  end
+
+    def average_star_rating
+      ##do the averages, all ratings over number of ratings the restaurant has.
+    end
+
+    def longest_review
+      #I would get all reviews for the restaurant, then .count, then sort by count
+    end
+
 
 end
