a568ae00e4753df25b60dcf81ad47a71f02ae4f6
github username <github email address>
2018-07-27 10:33:52 -0400

    IF I had 15 more minutes I would have killed it bro -_-

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,35 @@
 class Review
-  
-end
+  attr_reader :customer, :restaurant, :review
 
+@@all = []
+
+  def initialize(customer, restaurant, review = "", rating= nil)
+    @customer = customer
+    @restaurant = restaurant
+    @review = review
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+  def customer
+    Review.all.select do |review|
+    self.review == review.review
+
+  end
+  end
+
+# for the rating we would iterate through the our Review.all then select the instance aka element that matches that instance.rating
+
+
+#  for whatever review is caling .content we will find that revie
+
+
+
+
+end
