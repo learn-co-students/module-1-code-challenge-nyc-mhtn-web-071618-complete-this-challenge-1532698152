f40d958f51383e9c764d2b40d669fc8838db9537
github username <github email address>
2018-07-27 10:31:06 -0400

    moneyyyyy

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,64 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+
+    @@all.find do |customer|
+       customer.first_name == name.first_name && customer.last_name == name.last_name
+      end
+    end
+
+    def self.find_by_first_name(name)
+      @@all.select do |customer|
+        customer.first_name = name.first_name
+      end
+    end
+
+    def self.all_names
+      all = []
+        @@all.map do |customer|
+         all << customer.first_name + " " + customer.last_name
+      end
+      all.flatten.uniq
+    end
+#(customer, restaurant, rating, content)
+    def add_review(restaurant, rating, content)
+      Review.new(self, restaurant,rating, content)
+    end
+    #helper methods
+    def reviews
+      Review.all.select{|review| review.customer == self}
+    end
+
+    def num_reviews
+      reviews.count
+    end
+
+    # def num_reviews
+    #    Review.all.select{|review| review.customer == self}.count
+    #  end
+
+    def restaurants
+      reviews.map do |a_review|
+        a_review.restaurant
+      end
+    end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,27 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+jin = Customer.new("Jin", "Li")
+jinl = Customer.new("Jin", "Loo")
+jinb = Customer.new("Jin", "Boo")
+angel = Customer.new("Angel", "Tsang")
+boba = Customer.new("Boba", "Li")
+
+sophie = Restaurant.new("sophie")
+deli = Restaurant.new("deli")
+wangtron = Restaurant.new("wangtron")
+foodcourt = Restaurant.new("foodcourt")
+mcd = Restaurant.new("mcd")
+
+#(customer, restaurant, rating, content)
+r1 = Review.new(jin, sophie, 1, "its greeeattttt")
+r2 = Review.new(angel, wangtron, 5, "WANGTRON")
+r3 = Review.new(boba, foodcourt, 1, "greeeattttt")
+r4 = Review.new(jinl, mcd, 5, "fast food")
+r5 = Review.new(jinb, sophie, 3, "its greeeattttt")
+r6 = Review.new(jin, sophie, 2, "its horribad")
+r7 = Review.new(angel, sophie, 1, "what is this")
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
