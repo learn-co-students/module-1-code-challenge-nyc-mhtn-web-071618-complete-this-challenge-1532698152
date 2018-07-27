1a6d4bc849c1f86b21ae59c54ecb4d8d5707cfc8
cmcorrales <cmcorrales3@gmail.com>
2018-07-27 10:34:52 -0400

    module1 challenge complete

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,60 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
-    "#{first_name} #{last_name}"
+    "#{@first_name} #{@last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+# should return an **array** of all of the customer full names
+  def self.all_names
+    self.all.map do |customer|
+      first = customer.first_name
+      last = customer.last_name
+      "#{first} #{last}"
+    end
+  end
+
+# given a string of a **full name**, returns the **first customer** whose full name matches
+  def self.find_by_name(full_name)
+    self.all.select do |customer|
+      customer.all_names == full_name
+    end
+  end
+
+#  given a **restaurant object**, some review content (as a string), and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+  def add_review(restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
+  end
+
+#  Returns the total number of reviews that a customer has authored
+
+  def num_reviews
+    customer_reviews_arr = Review.all.select do |review|
+      review.customer == self
+    end
+    customer_reviews_arr.size
+  end
+
+#   Returns a **unique** array of all restaurants a customer has reviewed
+  def restaurants
+    customer_retaurants_arr = Review.all.select do |review|
+      review.restaurant == self
+    end
+    customer_retaurants_arr.uniq
+  end
+
+
 end
+
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,55 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(restaurant_name)
+    self.all.find do |restaurant|
+      restaurant
+    end
+  end
+
+#  returns an array of all reviews for that restaurant
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+# Returns a **unique** list of all customers who have reviewed a particular restaurant.
+  def customers
+    customer_array = reviews.map do |review|
+      review.customer
+    end
+    customer_array.uniq
+  end
+
+# returns the average star rating for a restaurant based on its reviews
+  def average_star_rating
+    ratings_array = reviews.map do |review|
+      review.rating
+    end
+    ratings_array.reduce(:+) / ratings_array.size
+  end
+
+#  returns the longest review content for a given restaurant
+  def longest_review
+    string_letters_count = 0
+    content_array = reviews.map do |review|
+      review.content
+    end
+    content_array.select do |content|
+      content.size.max
+    end
   end
 
 end
