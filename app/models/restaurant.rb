66896d09a0bdbc90dfffdba747b087d0f91f4700
github username <github email address>
2018-07-27 10:33:40 -0400

    donezo

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,27 @@
 class Restaurant
+  @@all = []
   attr_accessor :name
 
   def initialize(name)
     @name = name
+    @@all << self
+  end #end init
+
+  def self.all
+    @@all
   end
 
-end
+  def self.find_by_name(name)
+    @@all.find do |restaurant|
+      restaurant.name == name
+    end
+  end #end findbyname
+
+  def average_star_rating
+  end #end avg_star
+
+  def longest_review
+
+  end #end lognest_review
+
+end #end restaurant class
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,37 @@
 class Review
-  
-end
+  @@all = []
+  attr_reader :customer, :restaurant
+  attr_accessor :rating, :content
 
+  def initialize(content, restaurant, customer, rating)
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @customer = customer
+    @@all << self
+  end #end init
+
+  def self.all
+    @@all
+  end #end .all
+
+  def customer
+    binding.pry
+    self.map {|review| review.customer}
+  end #end customer
+
+  def restaurant
+    #self same as customer but with restauant
+
+  end #end resrtaurant
+
+  def rating
+    #self same as customer but with rating
+  end #end rating
+
+  def content
+    #self sameas customer but wth content
+
+  end #end content
+
+end #end review class
